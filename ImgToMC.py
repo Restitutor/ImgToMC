090fa402c3fc673129a9c82b300bf9fa676e89e6
vulpic <66442608+vulpic@users.noreply.github.com>
2023-02-13 22:14:28 -0600

    Align the rows of text (#5)
    adding a space character at the start lines up the rows with each other.

--- ImgToMC.py
+++ ImgToMC.py
@@ -38,7 +38,7 @@ def onfimHandler(link):
 
 def generateTellraw(hexList):
     out = []
-    out.append('tellraw @a [""')
+    out.append('tellraw @a [" "')
     for x in hexList:
         out.append(",")
         out.append(r'{"text":"#","color":"#' + x + r'"}')
